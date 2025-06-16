46d1d77c35e90a73d3ebf846f6129bc6b170d55c
Faiyaz Khira <faiyazkhira27@gmail.com>
2025-06-16 14:14:37 +0530

    Initial commit: Spring Boot project with .gitignore and properties template

---
+++ src/test/java/com/faiyaz/project/fittrack/FittrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faiyaz.project.fittrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
