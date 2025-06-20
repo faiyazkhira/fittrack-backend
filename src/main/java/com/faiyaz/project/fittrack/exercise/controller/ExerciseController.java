c71eeae7d151c756b931a1aae14a793059e019c2
Faiyaz Khira <faiyazkhira27@gmail.com>
2025-06-20 11:32:24 +0530

    Implement exercise progress tracking with volume and date filter

--- src/main/java/com/faiyaz/project/fittrack/exercise/controller/ExerciseController.java
+++ src/main/java/com/faiyaz/project/fittrack/exercise/controller/ExerciseController.java
@@ -1,15 +1,19 @@
 package com.faiyaz.project.fittrack.exercise.controller;
 
+import com.faiyaz.project.fittrack.exercise.dto.ExerciseProgressResponseDto;
 import com.faiyaz.project.fittrack.exercise.dto.ExerciseRequestDto;
 import com.faiyaz.project.fittrack.exercise.dto.ExerciseResponseDto;
 import com.faiyaz.project.fittrack.exercise.dto.ExerciseUpdateRequestDto;
 import com.faiyaz.project.fittrack.exercise.service.ExerciseService;
 import com.faiyaz.project.fittrack.user.entity.User;
+import org.apache.coyote.Response;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -42,5 +46,18 @@ public class ExerciseController {
                                             @AuthenticationPrincipal User user) throws AccessDeniedException {
         exerciseService.deleteExercise(id, user.getId());
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/progress")
+    public ResponseEntity<List<ExerciseProgressResponseDto>> getExerciseProgress(
+            @AuthenticationPrincipal User user,
+            @RequestParam String name,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate endDate
+    ) {
+        List<ExerciseProgressResponseDto> response = exerciseService.getExerciseProgress(user.getId(), name, startDate, endDate);
+        return ResponseEntity.ok(response);
     }
 }
